e0745e77b9b786d93aae0a58f6e13441f96ed9a2
java-tester-x <iohonko@gmail.com>
2014-07-02 20:42:37 +0300

    Continue work on MyDate class

--- src/MyDate.java
+++ src/MyDate.java
@@ -3,6 +3,12 @@ package src;
 
 public class MyDate {
 
+    private static final int MIN_YEAR  = 1;
+    private static final int MAX_YEAR  = 9999;
+
+    private static final int MIN_MONTH = 1;  // human friendly
+    private static final int MAX_MONTH = 12; // human friendly
+
     private int year;
     private int month;
     private int day;
@@ -11,29 +17,75 @@ public class MyDate {
                                     ,"Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
 
     private String[] strDays      = {"Sunday", "Monday", "Tuesday", "Wednesday"
-                                    ,"Thursday", "Friday", "Saturday", "Sunday"};
+                                    ,"Thursday", "Friday", "Saturday"};
 
-    private int[]    daysInMonths = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    private static final int[] daysInMonths            = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    private static final int[] nonLeapYearMonthNumbers = {0, 3, 3, 6, 1, 4, 6, 2, 5, 0, 3, 5};    
+    private static final int[] leapYearMonthNumbers    = {6, 2, 3, 6, 1, 4, 6, 2, 5, 0, 3, 5};
 
 
     public MyDate(int year, int month, int day) {
         setDate(year, month, day);
     }
 
+    /**
+     * A year is a leap year if it is divisible by 4 but not by 100,
+     * or it is divisible by 400.
+     * 
+     * @param  year [description]
+     * @return      [description]
+     */
     public static boolean isLeapYear(int year) {
-        return false;
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
     }
 
     public static boolean isValidDate(int year, int month, int day) {
-        // Between 1 to 9999
-        return false;
+        int dayMax = daysInMonths[month-1] + (isLeapYear(year) && month == 2 ? 1 : 0 );
+        return (MIN_YEAR  <= year   && year  <= MAX_YEAR)
+            && (MIN_MONTH <= month  && month <= MAX_MONTH)
+            && (1         <= day    && day   <= dayMax);
     }
 
-    public static int getDayOfWeek(int year, int month, int day) {
-        return 0;
+    /**
+     * Returns the day of the week, where 0 for Sun, 1 for Mon, ..., 6 for Sat,
+     * for the given date. Assume that the date is valid.
+     * 
+     * @param  year  [description]
+     * @param  month [description]
+     * @param  day   [description]
+     * @return       [description]
+     */
+    public static int getDayOfWeek(int year, int month, int day)
+    {
+        if (! isValidDate(year, month, day)) {
+            return -1;
+        }
+
+        // 1. Based on the first two digit of the year, get the number from the "century" table.
+        int magicCenturyNumber = 6 - 2*((year / 100) % 4);
+        
+        // 2. Add to the last two digit of the year.
+        int lastTwoDigitsOfYear = year % 100;
+        
+        // 3. Add to "the last two digit of the year divide by 4, truncate the fractional part".
+        int magicYearNumber = lastTwoDigitsOfYear / 4;
+        
+        // 4. Add to the number obtained from the month table.
+        int magicMonthNumber = isLeapYear(year) ? leapYearMonthNumbers[month-1] : nonLeapYearMonthNumbers[month-1];
+        
+        // 5. Add to the day.
+        int magicDayNumber = day;
+        
+        // 6. The sum modulus 7 gives the day of the week, where 0 for SUN, 1 for MON, ..., 6 for SAT.
+        return (  magicCenturyNumber + lastTwoDigitsOfYear 
+                + magicYearNumber    + magicMonthNumber + magicDayNumber) % 7; 
     }
 
-    public void setDate(int year, int month, int day) {
+    public void setDate(int year, int month, int day)
+    {
+        if (! isValidDate(year, month, day)) {
+            throw new IllegalArgumentException("Invalid year, month, or day!");
+        }
         this.year  = year;
         this.month = month;
         this.day   = day;
@@ -51,23 +103,26 @@ public class MyDate {
         return this.day;
     }
 
-    public void setYear(int year) {
-        if (year < 1 || year > 9999) {
+    public void setYear(int year)
+    {
+        if (year < MIN_YEAR || year > MAX_YEAR) {
             throw new IllegalArgumentException("Invalid year!");
         }
         this.year = year;
     }
 
-    public void setMonth(int month) {
-        if (month < 1 || month > 12) {
+    public void setMonth(int month)
+    {
+        if (month < MIN_MONTH || month > MAX_MONTH) {
             throw new IllegalArgumentException("Invalid month!");
         }
         this.month = month;
     }
 
-    public void setDay(int day) {
-        int monthDay = 31; 
-        if (day < 1 || day > monthDay) {
+    public void setDay(int day) 
+    {
+        int dayMax = daysInMonths[month-1] + (isLeapYear(year) && month == 2 ? 1 : 0 );
+        if (day < 1 || day > dayMax) {
             throw new IllegalArgumentException("Invalid day!");
         }
         this.day = day;
@@ -97,10 +152,11 @@ public class MyDate {
         return this;
     }
 
+    /**
+     * @return date in "xxxday d mmm yyyy", e.g., "Tuesday 14 Feb 2012" format.
+     */
     public String toString() {
-        // "xxxday d mmm yyyy", e.g., "Tuesday 14 Feb 2012".
-        return String.format("%1$s %2$d %3$s %2$d"
-            , strDays[day-1], day, strMonths[month-1], year
-        );
+        int weekDay = getDayOfWeek(year, month, day);
+        return String.format("%1$s %2$d %3$s %4$d", strDays[weekDay], day, strMonths[month-1], year);
     }
 }   
--- src/TestMyDate.java
+++ src/TestMyDate.java
@@ -4,6 +4,27 @@ public class TestMyDate {
 
     public static void main(String[] args)
     {
-        // 
+        MyDate currentDate = new MyDate(2014, 7, 2);
+        System.out.println("Current date: " + currentDate);
+
+        MyDate d1 = new MyDate(2012, 2, 28);
+        System.out.println(d1);             // Tuesday 28 Feb 2012
+        System.out.println(d1.nextDay());   // Wednesday 29 Feb 2012
+        System.out.println(d1.nextDay());   // Thursday 1 Mar 2012
+        System.out.println(d1.nextMonth()); // Sunday 1 Apr 2012
+        System.out.println(d1.nextYear());  // Monday 1 Apr 2013
+
+        MyDate d2 = new MyDate(2012, 1, 2);
+        System.out.println(d2);                 // Monday 2 Jan 2012
+        System.out.println(d2.previousDay());   // Sunday 1 Jan 2012
+        System.out.println(d2.previousDay());   // Saturday 31 Dec 2011
+        System.out.println(d2.previousMonth()); // Wednesday 30 Nov 2011
+        System.out.println(d2.previousYear());  // Tuesday 30 Nov 2010
+
+        MyDate d3 = new MyDate(2012, 2, 29);
+        System.out.println(d3.previousYear());  // Monday 28 Feb 2011
+
+        // MyDate d4 = new MyDate(2099, 11, 31); // Invalid year, month, or day!
+        // MyDate d5 = new MyDate(2011, 2, 29);  // Invalid year, month, or day!
     }
 }   
