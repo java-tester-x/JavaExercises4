e207e727e9445315425c0289239b99ecebe4f628
java-tester-x <iohonko@gmail.com>
2014-08-20 19:39:43 +0300

    Exercise: The Discount System

---
+++ src/ex7/DiscountSystem/CustomerTest.java
@@ -0,0 +1,42 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class CustomerTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class CustomerTest
+{
+    /**
+     * Default constructor for test class CustomerTest
+     */
+    public CustomerTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @Before
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @After
+    public void tearDown()
+    {
+    }
+}
