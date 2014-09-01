e729d99f7d1cef067f3b28fd85ccfd3d088d3fac
java-tester-x <iohonko@gmail.com>
2014-09-01 19:53:22 +0300

    Reorganize code of src/ex7

---
+++ src/ex7/Visit.java
@@ -0,0 +1,48 @@
+
+import java.util.Date;
+
+/**
+ * The Discount System: Visit.
+ */
+public class Visit
+{
+    private Customer customer;
+    private Date date;
+    private double serviceExpense;
+    private double productExpense;
+    
+    public Visit(String name, Date date)
+    {
+        this.customer   = new Customer(name);
+        this.date       = date;
+    }
+    
+    public String getName() {
+        return customer.getName();
+    }
+    
+    public double getServiceExpense() {
+        return serviceExpense;
+    }
+    
+    public void setServiceExpense(double ex) {
+        serviceExpense = ex;
+    }
+
+    public double getProductExpense() {
+        return productExpense;
+    }
+    
+    public void setProductExpense(double ex) {
+        productExpense = ex;
+    }
+
+    public double getTotalExpense() {
+        return serviceExpense + productExpense;
+    }
+    
+    public String toString() {
+        return String.format("Visit of customer %1$s at date %2$s"
+                            , customer.toString(), date.toString());
+    }
+}
