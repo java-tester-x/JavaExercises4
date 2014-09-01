4593373c57f756173bfd5d4e48afe020b3163c76
java-tester-x <iohonko@gmail.com>
2014-09-01 19:51:06 +0300

    Exercise: Polyline of Points with ArrayList

--- src/ex7/DiscountSystem/Discount.java
+++ src/ex7/DiscountSystem/Discount.java
@@ -1,33 +1,32 @@
-
 /**
- * Write a description of class Discount here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
  */
 public class Discount
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Discount
-     */
-    public Discount()
-    {
-        // initialise instance variables
-        x = 0;
+    private static double serviceDiscountPremium = 0.2;
+    private static double serviceDiscountGold    = 0.15;
+    private static double serviceDiscountSilver  = 0.1;
+    private static double productDiscountPremium = 0.1;
+    private static double productDiscountGold    = 0.1;
+    private static double productDiscountSilver  = 0.1;
+    
+    private Discount() {}
+    
+    public static double getServiceDiscountRate(String type) {
+        switch (type) {
+            case "PREMIUM": return serviceDiscountPremium;
+            case "GOLD":    return serviceDiscountGold;
+            case "SILVER":  return serviceDiscountSilver;
+        }
+        return 0.0;
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    
+    public static double getProductDiscountRate(String type) {
+        switch (type) {
+            case "PREMIUM": return productDiscountPremium;
+            case "GOLD":    return productDiscountGold;
+            case "SILVER":  return productDiscountSilver;
+        }
+        return 0.0;
     }
 }
